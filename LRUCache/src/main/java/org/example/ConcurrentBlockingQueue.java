ed6fde05cc889fd1040da5de8f3205b0945b96d5
galt-k <revanth.katanguri@gmail.com>
2026-07-20 10:44:19 -0700

    CBQ-v3-independent locking on either sides of tail and head

--- LRUCache/src/main/java/org/example/ConcurrentBlockingQueue.java
+++ LRUCache/src/main/java/org/example/ConcurrentBlockingQueue.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -12,49 +13,60 @@ class Node {
 }
 
 public class ConcurrentBlockingQueue {
-    private int size;
+    private AtomicInteger size = new AtomicInteger(0);
     private Node head;
     private Node tail;
     private int capacity;
     //create a new Reentrant lock
-    ReentrantLock lock = new ReentrantLock(true); // I wanted it to be a fair chance.
+    ReentrantLock headLock = new ReentrantLock(true); // I wanted it to be a fair chance.
+    ReentrantLock tailLock = new ReentrantLock(true); // I wanted it to be a fair chance.
     //create a new condition lock
-    Condition notFull = lock.newCondition();
-    Condition notEmpty = lock.newCondition();
+    Condition notFull = tailLock.newCondition();
+    Condition notEmpty = headLock.newCondition();
 
     public ConcurrentBlockingQueue(int capacity) {
-        this.size = 0;
-        this.head = null;
-        this.tail = null;
+        this.size.set(0);
+        Node dummy = new Node(-1);
+        this.head = dummy;
+        this.tail = dummy;
         this.capacity = capacity;
     }
     // create a new node and update the tail pointer
+    // used by producers, always a tail operation.
     public void put(int value) throws InterruptedException {
         //here add a lock
-        lock.lock();
+        int countBeforeInsertion = -1;
+        tailLock.lock();
         try {
-            while (this.size == this.capacity) {
+            while (this.size.get() == this.capacity) {
                 //here i want to ensure the thread goes into the blocking state and
-                notFull.await();
+                notFull.await(); // waiting for the ideal condition to occur. The ideal condition is the queue to be notFull.
             }
 
             Node newNode = new Node(value);
-            this.size++;
+            countBeforeInsertion = this.size.getAndIncrement();
+
             // FIXED: Clean branch ensuring we don't double-link a node to itself
-            if (this.head == null) {
-                this.head = newNode;
-                this.tail = newNode;
-            } else {
-                this.tail.next = newNode;
-                this.tail = newNode;
+            this.tail.next = newNode;
+            this.tail = newNode;
+
+            if(countBeforeInsertion + 1 < this.capacity) {
+                notFull.signal();
             }
 
-            //Tell a sleeping consumer that data is available
-            notEmpty.signalAll();
         } finally {
-            lock.unlock();
+            tailLock.unlock();
         }
 
+        if(countBeforeInsertion == 0) {
+            //wake up the consumer threads
+            headLock.lock();
+            try {
+                notEmpty.signal();
+            } finally {
+                headLock.unlock();
+            }
+        }
         return;
     }
 
@@ -62,28 +74,42 @@ public class ConcurrentBlockingQueue {
         // return the value in the head
         // update the head pointer
         // should also update the size
-        lock.lock();
+        int result;
+        int countBeforeInsertion = -1;
+        headLock.lock();
         try {
-            while (this.head == null) {
+            while (this.head.next == null) {
+                // if the queue is empty, the threads should go into waiting mode.
                 notEmpty.await();
             }
-            int result;
-            if (this.head == this.tail) {
-                result = this.head.value;
-                this.head = null;
-                this.tail = null;
-            } else {
-                result = this.head.value;
-                Node temp = this.head;
-                this.head = this.head.next;
-                temp.next = null;
+            // the goal here is to not use tail.
+            Node firsRealNode = this.head.next;
+            result= firsRealNode.value;
+
+            this.head = firsRealNode;
+
+
+            //decrement the size by fetching the old value
+            countBeforeInsertion = this.size.getAndDecrement();
+
+            // if there are still items left? wake up the next consumer threads
+            if(countBeforeInsertion > 1 ) {
+                notEmpty.signal();
             }
-            this.size--;
-            notFull.signalAll();
-            return result;
         } finally {
-            lock.unlock();
+            headLock.unlock();
         }
+        //cross lock notification
+        if(countBeforeInsertion == capacity) {
+            tailLock.lock();
+            try {
+                notFull.signal();
+            } finally {
+                tailLock.unlock();
+            }
+        }
+
+        return result;
 
     }
 }
